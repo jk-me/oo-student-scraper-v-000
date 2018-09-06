76ac420b84710a8c367a8e99721b51cd5d3cd646
Jenny K <jkam17@gmail.com>
2018-09-06 01:54:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,11 +5,16 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    @name=student_hash[:name]
+    @location=student_hash[:location]
+    @profile_url=student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each{|x|
+      Student.new(x) 
+    }
   end
 
   def add_student_attributes(attributes_hash)
@@ -17,7 +22,31 @@ class Student
   end
 
   def self.all
-    
+    @@all
   end
 end
 
+    # Scraper.scrape_index_page(index_url)
+    # # => [
+    #         {:name => "Abby Smith", :location => "Brooklyn, NY", :profile_url => "students/abby-smith.html"},
+    #         {:name => "Joe Jones", :location => "Paris, France", :profile_url => "students/joe-jonas.html"},
+    #         {:name => "Carlos Rodriguez", :location => "New York, NY", :profile_url => "students/carlos-rodriguez.html"},
+    #         {:name => "Lorenzo Oro", :location => "Los Angeles, CA", :profile_url => "students/lorenzo-oro.html"},
+    #         {:name => "Marisa Royer", :location => "Tampa, FL", :profile_url => "students/marisa-royer.html"}
+    #       ]
+          
+    # Scraper.scrape_profile_page(profile_url)
+    # # => {:twitter=>"http://twitter.com/flatironschool",
+    #       :linkedin=>"https://www.linkedin.com/in/flatironschool",
+    #       :github=>"https://github.com/learn-co,
+    #       :blog=>"http://flatironschool.com",
+    #       :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+    #       :bio=> "I'm a school"
+    #     }
+          
+          
+          
+          
+          
+          
+          
