8e39db3f76bb6c52055fcc1d794d2fbef98334cf
Jenny K <jkam17@gmail.com>
2018-09-06 02:38:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,14 +26,6 @@ class Student
   end
 end
 
-    # Scraper.scrape_index_page(index_url)
-    # # => [
-    #         {:name => "Abby Smith", :location => "Brooklyn, NY", :profile_url => "students/abby-smith.html"},
-    #         {:name => "Joe Jones", :location => "Paris, France", :profile_url => "students/joe-jonas.html"},
-    #         {:name => "Carlos Rodriguez", :location => "New York, NY", :profile_url => "students/carlos-rodriguez.html"},
-    #         {:name => "Lorenzo Oro", :location => "Los Angeles, CA", :profile_url => "students/lorenzo-oro.html"},
-    #         {:name => "Marisa Royer", :location => "Tampa, FL", :profile_url => "students/marisa-royer.html"}
-    #       ]
           
     # Scraper.scrape_profile_page(profile_url)
     # # => {:twitter=>"http://twitter.com/flatironschool",
